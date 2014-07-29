72fd87f573a1aa925aa7ac148afb2f3aeb6b81b9
jualoppaz <juanmalp1992@gmail.com>
2014-07-29 20:23:10 +0200

    Implementado el css de una primera version de busqueda de caramelos.

---
+++ app/public/js/angular/buscar.js
@@ -0,0 +1,20 @@
+var buscar = angular.module('buscar', []);
+
+function mainController($scope, $http) {
+    $scope.caramelos = {};
+    $scope.loguedUser = {};
+
+    // Cuando se cargue la página, pide del API todas las excursiones
+    $http.get('/api/conPalo')
+        .success(function(data) {
+            if(data.message){
+                $scope.conPalos = {};
+            }else{
+                $scope.conPalos = data;
+            }
+        })
+        .error(function(data) {
+            alert("No se ha podido cargar la página correctamente. Recargue la página. Gracias.");
+        });
+
+}
