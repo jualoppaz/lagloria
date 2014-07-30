f92c8dff094efa1f4d4ae0e0d435154191366acd
jualoppaz <juanmalp1992@gmail.com>
2014-07-30 20:30:15 +0200

    Solucionados los pequenos problemas de logica de los selects, de modo que se han asociado todos los inputs y selects a la variable final mediante la directiva ng-model.

--- app/public/js/angular/buscar.js
+++ app/public/js/angular/buscar.js
@@ -4,10 +4,10 @@ function mainController($scope, $http) {
     $scope.caramelos = {};
     $scope.loguedUser = {};
 
-    $scope.seccionSeleccionada = "Toda la web";
-    $scope.criterioSeleccionado = "Nombre";
-    $scope.categoriaSeleccionada = "---";
-    $scope.nombreIntroducido = "";
+    $scope.seccionSeleccionada = {name:'Toda la web'};
+    $scope.criterioSeleccionado = {name: 'Nombre'};
+    $scope.categoriaSeleccionada = {name: '---'};
+    $scope.nombreIntroducido = {name: ''};
 
     $scope.sections = [
         {name: 'Toda la web'},
@@ -27,34 +27,21 @@ function mainController($scope, $http) {
         {name: 'Con palo'}
     ];
 
-    $scope.actualizarSeccion = function(){
-        var select = angular.element("#selectSeccionABuscar option:selected");
-        $scope.seccionSeleccionada = select.text();
-    };
-
     $scope.actualizarCriterio = function(){
         var select = angular.element("#selectCriterioDeBusqueda option:selected");
-        /*alert("Criterio seleccionado: " + criterio.val());
-        alert("Criterio seleccionado: " + criterio.text());*/
         var criterio = select.text();
         if(criterio == "Nombre"){
-            $scope.categoriaSeleccionada = "---";
+
         }else if(criterio == "Categoría"){
-            $scope.categoriaSeleccionada = "Toffees y Masticables";
-            $scope.nombreIntroducido = "";
+            $scope.nombreIntroducido.name = "";
         }
-        $scope.criterioSeleccionado = select.text();
     };
 
-    $scope.actualizarCategoria = function(){
-        var select = angular.element("#selectCategoria option:selected");
-        $scope.categoriaSeleccionada = select.text();
-    };
-
+    /*
     $scope.actualizarNombre = function(){
         var input = angular.element("#inputNombre");
         //alert("Input: " + input.val());
-    };
+    };*/
 
     // Cuando se cargue la página, pide del API todas las excursiones
     $http.get('/api/conPalo')
