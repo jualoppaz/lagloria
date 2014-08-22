690b81a02cb1a1838aeb02c058d8dde7a40376c6
jualoppaz <juanmalp1992@gmail.com>
2014-08-22 17:12:14 +0200

    Implementado el carousel en la vista del surtido de masticables.

--- app/public/js/angular/toffeesYMasticables/masticables/info.js
+++ app/public/js/angular/toffeesYMasticables/masticables/info.js
@@ -2,6 +2,8 @@ var app = angular.module('lagloria');
 
 app.controller('InfoMasticableController', function($scope, $http) {
     $scope.producto = {};
+    $scope.numeros = [];
+
 
     var url = window.location.href.split("/");
     var masticableId = url[url.length - 1];
@@ -10,6 +12,10 @@ app.controller('InfoMasticableController', function($scope, $http) {
     $http.get('/api/masticables/' + String(masticableId))
         .success(function(data) {
             $scope.producto = data;
+            for(i=0;i<$scope.producto.avaibleSavours.length; i++){
+                $scope.numeros[i] = i+1;
+            }
+
         })
         .error(function(data) {
             alert("No se ha podido cargar la página correctamente. Recargue la página. Gracias.");
